f3a356434b52b8ef7437560faa8d6d510d489e0a
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-11-03 00:01:05 -0400

    continued working on js, encountered bug with preventdefault

--- assets/java.js
+++ assets/java.js
@@ -1,16 +1,29 @@
-var firebaseConfig = {
+let firebaseConfig = {
     apiKey: "AIzaSyCIxB13rG3OuJ6bbv-N4A8ETJpBEONpbHs",
     databaseURL: "https://chugga-chugga.firebaseio.com",
   };
 
-  firebase.initializeApp(firebaseConfig);
-  var database = firebase.database;
+firebase.initializeApp(firebaseConfig);
+let database = firebase.database;
 
+let trainName="";
+let destination="";
+let firstTime="";
+let frequency="";
 
-
-$("#trainSubmit").on("click", function(event) {
+$("#addTrain").on("click", function(event) {
   event.preventDefault()
-  let trainName = $("#trainName").val().trim()
-  console.log(trainName)
+  let trainName = $("#trainName").val().trim();
+  $("#trainName").val("");
+  let destination = $("#destination").val().trim();
+  $("#destination").val("");
+  let firstTime = $("#firstTrainTime").val().trim();
+  $("#firstTrainTime").val("");
+  let frequency = $("#frequency").val().trim(); 
+  $("#frequency").val("");
+  console.log(trainName);
+  console.log(destination);
+  console.log(firstTime);
+  console.log(frequency);
   console.log("Working")
 });
