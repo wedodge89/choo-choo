77bd96f808ea062df486cf3bbf5b7f58eed29590
wedodge89 <50926033+wedodge89@users.noreply.github.com>
2019-11-05 14:17:46 -0500

    fixed preventdefault bug

--- assets/java.js
+++ assets/java.js
@@ -1,7 +1,7 @@
 let firebaseConfig = {
-    apiKey: "AIzaSyCIxB13rG3OuJ6bbv-N4A8ETJpBEONpbHs",
-    databaseURL: "https://chugga-chugga.firebaseio.com",
-  };
+  apiKey: "AIzaSyCIxB13rG3OuJ6bbv-N4A8ETJpBEONpbHs",
+  databaseURL: "https://chugga-chugga.firebaseio.com",
+};
 
 firebase.initializeApp(firebaseConfig);
 let database = firebase.database;
@@ -12,18 +12,18 @@ let firstTime="";
 let frequency="";
 
 $("#addTrain").on("click", function(event) {
-  event.preventDefault()
-  let trainName = $("#trainName").val().trim();
-  $("#trainName").val("");
-  let destination = $("#destination").val().trim();
-  $("#destination").val("");
-  let firstTime = $("#firstTrainTime").val().trim();
-  $("#firstTrainTime").val("");
-  let frequency = $("#frequency").val().trim(); 
-  $("#frequency").val("");
-  console.log(trainName);
-  console.log(destination);
-  console.log(firstTime);
-  console.log(frequency);
-  console.log("Working")
+event.preventDefault()
+let trainName = $("#trainName").val().trim();
+let destination = $("#destination").val().trim();
+let firstTime = $("#firstTrainTime").val().trim();
+let frequency = $("#frequency").val().trim(); 
+console.log(trainName);
+console.log(destination);
+console.log(firstTime);
+console.log(frequency);
+console.log("Working")
+$("#trainName").val("");
+$("#destination").val("");
+$("#firstTrainTime").val("");
+$("#frequency").val("");
 });
